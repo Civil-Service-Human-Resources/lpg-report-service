808e72efe8b47b597487958909f5eb8806402274
mmackiewicz1 <m.mackiewicz@kainos.com>
2020-05-26 16:58:25 +0200

    Implemented fixes

--- src/main/java/uk/gov/cshr/report/repository/DbRepository.java
+++ src/main/java/uk/gov/cshr/report/repository/DbRepository.java
@@ -2,13 +2,11 @@ package uk.gov.cshr.report.repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.Instant;
 import java.time.LocalDate;
 import java.util.List;
 
 import javax.sql.DataSource;
 
-import uk.gov.cshr.report.domain.learnerrecord.ModuleRecord;
 import uk.gov.cshr.report.reports.ModuleReportRow;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,16 +15,17 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class DbRepository {
-    private static final String GET_LEARNER_RECORD_REPORT_DATA = "select i.email, i.uid, c.full_name, ou.name, p.name, g.name, group_concat(g.name), mr.module_id, mr.state, cr.user_id, mr.updated_at, mr.completion_date FROM learner_record.module_record mr " +
+    private static final String GET_LEARNER_RECORD_REPORT_DATA = "select i.email, i.uid, c.full_name, ou.name, p.name, g.name, group_concat(p2.name), mr.module_id, mr.state, cr.user_id, mr.updated_at, mr.completion_date FROM learner_record.module_record mr " +
         "LEFT JOIN learner_record.course_record cr ON cr.course_id = mr.course_id " +
         "INNER JOIN identity.identity i ON cr.user_id = i.uid " +
         "INNER JOIN csrs.civil_servant c ON i.id = c.identity_id " +
         "LEFT JOIN csrs.organisational_unit ou ON ou.id = c.organisational_unit_id " +
         "LEFT JOIN csrs.profession p ON p.id = c.profession_id " +
         "LEFT JOIN csrs.grade g ON g.id = c.grade_id " +
-        "JOIN csrs.civil_servant_other_areas_of_work oaw ON c.id = oaw.civil_servant_id AND p.id = oaw.other_areas_of_work_id " +
+        "LEFT JOIN csrs.civil_servant_other_areas_of_work oaw on c.id = oaw.civil_servant_id " +
+        "LEFT JOIN csrs.profession p2 on p2.id  = oaw.other_areas_of_work_id " +
         "WHERE mr.updated_at BETWEEN ? AND ? AND mr.course_id IS NOT NULL " +
-        "GROUP BY c.id";
+        "GROUP BY c.id, learner_record.mr.module_id, learner_record.mr.state, learner_record.cr.user_id, learner_record.mr.updated_at, learner_record.mr.completion_date";
 
     private final JdbcTemplate jdbcTemplate;
 
