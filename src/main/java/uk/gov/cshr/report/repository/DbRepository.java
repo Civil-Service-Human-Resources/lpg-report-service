7f6b28d0e5ea44eea1b2ee34b7db7b0035180ab6
mmackiewicz1 <m.mackiewicz@kainos.com>
2020-06-01 16:36:47 +0200

    Db calls one by one

---
+++ src/main/java/uk/gov/cshr/report/repository/DbRepository.java
@@ -0,0 +1,123 @@
+package uk.gov.cshr.report.repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.sql.DataSource;
+
+import uk.gov.cshr.report.domain.identity.Identity;
+import uk.gov.cshr.report.domain.learnerrecord.ModuleRecord;
+import uk.gov.cshr.report.domain.registry.CivilServant;
+import uk.gov.cshr.report.reports.ModuleReportRow;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DbRepository {
+    private static final String GET_IDENTITIES = "SELECT i.email, i.uid " +
+        "FROM identity.identity i";
+    private static final String GET_LEARNER_RECORDS = "SELECT mr.module_id, mr.state, cr.user_id, mr.updated_at, mr.completion_date " +
+        "FROM learner_record.module_record mr " +
+        "LEFT JOIN learner_record.course_record cr on cr.course_id = mr.course_id " +
+        "WHERE mr.updated_at BETWEEN ? AND ? AND mr.course_id IS NOT NULL";
+    private static final String GET_CIVIL_SERVANTS = "select c.id, c.full_name, ou.name, p.name, i.uid, g.name, group_concat(p2.name) " +
+        "FROM csrs.civil_servant c " +
+        "LEFT JOIN csrs.organisational_unit ou on ou.id = c.organisational_unit_id " +
+        "LEFT JOIN csrs.profession p on p.id = c.profession_id " +
+        "LEFT JOIN csrs.identity i on i.id = c.identity_id " +
+        "LEFT JOIN csrs.grade g on g.id = c.grade_id " +
+        "LEFT JOIN csrs.civil_servant_other_areas_of_work oaw on c.id = oaw.civil_servant_id " +
+        "LEFT JOIN csrs.profession p2 on p2.id  = oaw.other_areas_of_work_id " +
+        "group by c.id";
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public DbRepository(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public Map<String, Identity> getIdentities() {
+        return jdbcTemplate.query(GET_IDENTITIES, (rs, rowNum) -> extractIdentity(rs)).stream()
+            .collect(Collectors.toMap(Identity::getUid, identity -> identity));
+    }
+
+    public List<ModuleRecord> getModuleRecords(LocalDate from, LocalDate to) {
+        return jdbcTemplate.query(GET_LEARNER_RECORDS, (rs, rowNum) -> extractModuleRecord(rs), from, to);
+    }
+
+    public Map<String, CivilServant> getCivilServants() {
+        return jdbcTemplate.query(GET_CIVIL_SERVANTS, (rs, rowNum) -> extractCivilServant(rs)).stream()
+            .collect(Collectors.toMap(CivilServant::getEmail, civilServant -> civilServant));
+    }
+
+    private Identity extractIdentity(ResultSet rs) throws SQLException {
+        Identity identity = new Identity();
+        identity.setUsername(rs.getString(1));
+        identity.setUid(rs.getString(2));
+
+        return identity;
+    }
+
+    private ModuleRecord extractModuleRecord(ResultSet rs) throws SQLException {
+        ModuleRecord moduleRecord = new ModuleRecord();
+        moduleRecord.setModuleId(rs.getString(1));
+        moduleRecord.setState(rs.getString(2));
+        moduleRecord.setLearner(rs.getString(3));
+        moduleRecord.setStateChangeDate(rs.getString(4));
+        moduleRecord.setCompletedAt(rs.getString(5));
+
+        return moduleRecord;
+    }
+
+    private CivilServant extractCivilServant(ResultSet rs) throws SQLException {
+        CivilServant civilServant = new CivilServant();
+        civilServant.setId(rs.getString(1));
+        civilServant.setName(rs.getString(2));
+        civilServant.setOrganisation(rs.getString(3));
+        civilServant.setProfession(rs.getString(4));
+        civilServant.setEmail(rs.getString(5));
+        civilServant.setGrade(rs.getString(6));
+        civilServant.setOtherAreasOfWork(rs.getString(7));
+
+        return civilServant;
+    }
+
+    private ModuleReportRow extractModuleReportRow(ResultSet rs, boolean isProfessionReporter) throws SQLException {
+        ModuleReportRow reportRow = new ModuleReportRow();
+
+        if (!isProfessionReporter) {
+            reportRow.setEmail(rs.getString(1));
+            reportRow.setLearnerId(rs.getString(2));
+            reportRow.setName(rs.getString(3));
+        }
+
+        reportRow.setDepartment(rs.getString(4));
+        reportRow.setProfession(rs.getString(5));
+        reportRow.setGrade(rs.getString(6));
+        reportRow.setOtherAreasOfWork(rs.getString(7));
+
+        String state = rs.getString(9);
+        if (state != null) {
+            reportRow.setStatus(state);
+        }
+        reportRow.setLearnerId(rs.getString(10));
+        reportRow.setUpdatedAt(rs.getString(11));
+        reportRow.setCompletedAt(rs.getString(12));
+
+        reportRow.setModuleId(rs.getString(8));
+        reportRow.setModuleTitle(rs.getString(13));
+        reportRow.setModuleType(rs.getString(14));
+
+        reportRow.setCourseId(rs.getString(15));
+        reportRow.setCourseTitle(rs.getString(16));
+
+        return reportRow;
+    }
+}
--- src/test/java/uk/gov/cshr/report/service/ReportServiceTest.java
+++ src/test/java/uk/gov/cshr/report/service/ReportServiceTest.java
@@ -10,18 +10,22 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import uk.gov.cshr.report.domain.catalogue.Course;
 import uk.gov.cshr.report.domain.catalogue.Event;
+import uk.gov.cshr.report.domain.catalogue.Module;
 import uk.gov.cshr.report.domain.identity.Identity;
 import uk.gov.cshr.report.domain.learnerrecord.Booking;
+import uk.gov.cshr.report.domain.learnerrecord.BookingStatus;
+import uk.gov.cshr.report.domain.learnerrecord.ModuleRecord;
 import uk.gov.cshr.report.domain.registry.CivilServant;
 import uk.gov.cshr.report.factory.ReportRowFactory;
 import uk.gov.cshr.report.reports.BookingReportRow;
 import uk.gov.cshr.report.reports.ModuleReportRow;
-import uk.gov.cshr.report.repository.ModuleReportRowRepository;
+import uk.gov.cshr.report.repository.DbRepository;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -58,13 +62,19 @@ public class ReportServiceTest {
     private IdentityService identityService;
 
     @Mock
-    private ReportRowFactory reportRowFactory;
+    private DbRepository dbRepository;
 
-    @Mock
-    private ModuleReportRowRepository moduleReportRowRepository;
-
-    @InjectMocks
     private ReportService reportService;
+
+    @Before
+    public void initialize() {
+        reportService = new ReportService(learnerRecordService,
+            civilServantRegistryService,
+            learningCatalogueService,
+            new ReportRowFactory(),
+            identityService,
+            dbRepository);
+    }
 
     @Test
     @WithMockUser(username = "user", authorities = {"PROFESSION_AUTHOR"})
@@ -72,10 +82,12 @@ public class ReportServiceTest {
         Booking booking1 = new Booking();
         booking1.setEvent(EVENT_1);
         booking1.setLearner(LEARNER_1);
+        booking1.setStatus(BookingStatus.CONFIRMED);
 
         Booking booking2 = new Booking();
         booking2.setEvent(EVENT_2);
         booking2.setLearner(LEARNER_2);
+        booking2.setStatus(BookingStatus.CONFIRMED);
 
         CivilServant civilServant1 = new CivilServant();
         civilServant1.setId(LEARNER_1);
@@ -86,15 +98,26 @@ public class ReportServiceTest {
         CivilServant civilServant3 = new CivilServant();
         civilServant3.setId(LEARNER_3);
 
+        Course course1 = new Course();
+        course1.setId(COURSE_1);
+        Module module1 = new Module();
+        module1.setId(MODULE_1);
+        module1.setCourse(course1);
+        module1.setRequired(true);
+
         Event event = new Event();
         event.setId(EVENT_1);
+        event.setModule(module1);
 
         Identity identity = new Identity();
         identity.setUsername(EMAIL_1);
+        identity.setUid(LEARNER_1);
 
         LocalDate from = LocalDate.parse(DATE_FROM);
         LocalDate to = LocalDate.parse(DATE_TO);
 
+        when(identityService.getIdentitiesMap())
+            .thenReturn(ImmutableMap.of(identity.getUid(), identity));
         when(learnerRecordService.getBookings(from, to)).thenReturn(Arrays.asList(booking1, booking2));
         when(civilServantRegistryService.getCivilServantMap()).thenReturn(ImmutableMap.of(
                 LEARNER_1, civilServant1,
@@ -102,41 +125,63 @@ public class ReportServiceTest {
         ));
         when(learningCatalogueService.getEventMap()).thenReturn(ImmutableMap.of(EVENT_1, event));
 
-        BookingReportRow reportRow = new BookingReportRow();
-        when(reportRowFactory.createBookingReportRow(any(), any(), any(), any(), anyBoolean())).thenReturn(reportRow);
-
         List<BookingReportRow> result = reportService.buildBookingReport(from, to, false);
-
-        assertEquals(Collections.singletonList(reportRow), result);
+        assertEquals(result.size(), 1);
+        assertEquals(result.get(0).getEventID(), EVENT_1);
+        assertEquals(result.get(0).getModuleId(), MODULE_1);
+        assertEquals(result.get(0).getLearnerId(), LEARNER_1);
+        assertEquals(result.get(0).getEmail(), EMAIL_1);
+        assertEquals(result.get(0).getCourseId(), COURSE_1);
     }
 
     @Test
     @WithMockUser(username = "user", authorities = {"PROFESSION_AUTHOR"})
     public void shouldReturnModuleReport() {
-        ModuleReportRow moduleReportRow1 = new ModuleReportRow();
-        moduleReportRow1.setModuleId(MODULE_1);
-        moduleReportRow1.setLearnerId(LEARNER_1);
-        moduleReportRow1.setEmail(EMAIL_1);
-        moduleReportRow1.setModuleId(MODULE_1);
-        moduleReportRow1.setCourseId(COURSE_1);
+        Identity identity1 = new Identity();
+        identity1.setUsername(EMAIL_1);
+        identity1.setUid(LEARNER_1);
+        Identity identity2 = new Identity();
+        identity2.setUsername(EMAIL_2);
+        identity2.setUid(LEARNER_2);
 
-        ModuleReportRow moduleReportRow2 = new ModuleReportRow();
-        moduleReportRow2.setModuleId(MODULE_2);
-        moduleReportRow2.setLearnerId(LEARNER_2);
-        moduleReportRow2.setEmail(EMAIL_2);
-        moduleReportRow2.setModuleId(MODULE_2);
-        moduleReportRow2.setCourseId(COURSE_2);
+        ModuleRecord moduleRecord1 = new ModuleRecord();
+        moduleRecord1.setLearner(LEARNER_1);
+        moduleRecord1.setModuleId(MODULE_1);
+        ModuleRecord moduleRecord2 = new ModuleRecord();
+        moduleRecord2.setLearner(LEARNER_2);
+        moduleRecord2.setModuleId(MODULE_2);
 
-        List<ModuleReportRow> moduleReportRows = ImmutableList.of(moduleReportRow1, moduleReportRow2);
+        Course course1 = new Course();
+        course1.setId(COURSE_1);
+        Module module1 = new Module();
+        module1.setId(MODULE_1);
+        module1.setCourse(course1);
+        Course course2 = new Course();
+        course2.setId(COURSE_2);
+        Module module2 = new Module();
+        module2.setId(MODULE_2);
+        module2.setCourse(course2);
+
+        CivilServant civilServant1 = new CivilServant();
+        civilServant1.setId(LEARNER_1);
+        CivilServant civilServant2 = new CivilServant();
+        civilServant2.setId(LEARNER_2);
 
         LocalDate from = LocalDate.parse(DATE_FROM);
         LocalDate to = LocalDate.parse(DATE_TO);
 
-        when(moduleReportRowRepository.getModuleReportData(any(LocalDate.class), any(LocalDate.class), anyBoolean()))
-            .thenReturn(moduleReportRows);
+        when(dbRepository.getIdentities())
+            .thenReturn(ImmutableMap.of(identity1.getUid(), identity1, identity2.getUid(), identity2));
+        when(dbRepository.getModuleRecords(from, to))
+            .thenReturn(ImmutableList.of(moduleRecord1, moduleRecord2));
+        when(dbRepository.getCivilServants())
+            .thenReturn(ImmutableMap.of(civilServant1.getId(), civilServant1, civilServant2.getId(), civilServant2));
+        when(learningCatalogueService.getModuleMap())
+            .thenReturn(ImmutableMap.of(module1.getId(), module1, module2.getId(), module2));
 
-        List<ModuleReportRow> result = reportService.buildModuleReport(from, to, true);
+        List<ModuleReportRow> result = reportService.buildModuleReport(from, to, false);
 
+        assertEquals(result.size(), 2);
         assertEquals(result.get(0).getModuleId(), MODULE_1);
         assertEquals(result.get(0).getLearnerId(), LEARNER_1);
         assertEquals(result.get(0).getEmail(), EMAIL_1);
