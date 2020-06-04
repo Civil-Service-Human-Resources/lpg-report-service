c421f2cc1380bac6b3e7e243b24e9153d208d1c9
mmackiewicz1 <m.mackiewicz@kainos.com>
2020-06-04 11:38:51 +0200

    Refactor

--- src/main/java/uk/gov/cshr/report/repository/DbRepository.java
+++ src/main/java/uk/gov/cshr/report/repository/DbRepository.java
@@ -1,7 +1,5 @@
 package uk.gov.cshr.report.repository;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
@@ -11,10 +9,9 @@ import javax.sql.DataSource;
 
 import uk.gov.cshr.report.domain.identity.Identity;
 import uk.gov.cshr.report.domain.learnerrecord.Booking;
-import uk.gov.cshr.report.domain.learnerrecord.BookingStatus;
 import uk.gov.cshr.report.domain.learnerrecord.ModuleRecord;
 import uk.gov.cshr.report.domain.registry.CivilServant;
-import uk.gov.cshr.report.reports.ModuleReportRow;
+import uk.gov.cshr.report.factory.QueryResultExtractor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -47,101 +44,20 @@ public class DbRepository {
     }
 
     public List<ModuleRecord> getModuleRecords(LocalDate from, LocalDate to) {
-        return jdbcTemplate.query(GET_MODULE_RECORDS, (rs, rowNum) -> extractModuleRecord(rs), from, to);
+        return jdbcTemplate.query(GET_MODULE_RECORDS, (rs, rowNum) -> QueryResultExtractor.extractModuleRecord(rs), from, to);
     }
 
     public List<Booking> getBookings(LocalDate from, LocalDate to) {
-        return jdbcTemplate.query(GET_BOOKINGS, (rs, rowNum) -> extractBooking(rs), from, to);
+        return jdbcTemplate.query(GET_BOOKINGS, (rs, rowNum) -> QueryResultExtractor.extractBooking(rs), from, to);
     }
 
     public Map<String, Identity> getIdentitiesMap() {
-        return jdbcTemplate.query(GET_IDENTITIES, (rs, rowNum) -> extractIdentity(rs)).stream()
+        return jdbcTemplate.query(GET_IDENTITIES, (rs, rowNum) -> QueryResultExtractor.extractIdentity(rs)).stream()
             .collect(Collectors.toMap(Identity::getUid, identity -> identity));
     }
 
     public Map<String, CivilServant> getCivilServantMap() {
-        return jdbcTemplate.query(GET_CIVIL_SERVANTS, (rs, rowNum) -> extractCivilServant(rs)).stream()
+        return jdbcTemplate.query(GET_CIVIL_SERVANTS, (rs, rowNum) -> QueryResultExtractor.extractCivilServant(rs)).stream()
             .collect(Collectors.toMap(CivilServant::getEmail, civilServant -> civilServant));
-    }
-
-    private ModuleRecord extractModuleRecord(ResultSet rs) throws SQLException {
-        ModuleRecord moduleRecord = new ModuleRecord();
-        moduleRecord.setModuleId(rs.getString(1));
-        moduleRecord.setState(rs.getString(2));
-        moduleRecord.setLearner(rs.getString(3));
-        moduleRecord.setStateChangeDate(rs.getString(4));
-        moduleRecord.setCompletedAt(rs.getString(5));
-
-        return moduleRecord;
-    }
-
-    private Booking extractBooking(ResultSet rs) throws SQLException {
-        Booking booking = new Booking();
-        booking.setId(rs.getInt(1));
-        booking.setAccessibilityOptions(rs.getString(2));
-        booking.setBookingTime(rs.getString(3));
-        booking.setCancellationReason(rs.getString(4));
-        booking.setCancellationTime(rs.getString(5));
-        booking.setConfirmationTime(rs.getString(6));
-        booking.setEvent(rs.getString(7));
-        booking.setLearner(rs.getString(8));
-        booking.setPoNumber(rs.getString(9));
-        booking.setStatus(BookingStatus.forValue(rs.getString(10)));
-        booking.setBookingReference(rs.getString(11));
-
-        return booking;
-    }
-
-    private Identity extractIdentity(ResultSet rs) throws SQLException {
-        Identity identity = new Identity();
-        identity.setUsername(rs.getString(1));
-        identity.setUid(rs.getString(2));
-
-        return identity;
-    }
-
-    private CivilServant extractCivilServant(ResultSet rs) throws SQLException {
-        CivilServant civilServant = new CivilServant();
-        civilServant.setId(rs.getString(1));
-        civilServant.setName(rs.getString(2));
-        civilServant.setOrganisation(rs.getString(3));
-        civilServant.setProfession(rs.getString(4));
-        civilServant.setEmail(rs.getString(5));
-        civilServant.setGrade(rs.getString(6));
-        civilServant.setOtherAreasOfWork(rs.getString(7));
-
-        return civilServant;
-    }
-
-    private ModuleReportRow extractModuleReportRow(ResultSet rs, boolean isProfessionReporter) throws SQLException {
-        ModuleReportRow reportRow = new ModuleReportRow();
-
-        if (!isProfessionReporter) {
-            reportRow.setEmail(rs.getString(1));
-            reportRow.setLearnerId(rs.getString(2));
-            reportRow.setName(rs.getString(3));
-        }
-
-        reportRow.setDepartment(rs.getString(4));
-        reportRow.setProfession(rs.getString(5));
-        reportRow.setGrade(rs.getString(6));
-        reportRow.setOtherAreasOfWork(rs.getString(7));
-
-        String state = rs.getString(9);
-        if (state != null) {
-            reportRow.setStatus(state);
-        }
-        reportRow.setLearnerId(rs.getString(10));
-        reportRow.setUpdatedAt(rs.getString(11));
-        reportRow.setCompletedAt(rs.getString(12));
-
-        reportRow.setModuleId(rs.getString(8));
-        reportRow.setModuleTitle(rs.getString(13));
-        reportRow.setModuleType(rs.getString(14));
-
-        reportRow.setCourseId(rs.getString(15));
-        reportRow.setCourseTitle(rs.getString(16));
-
-        return reportRow;
     }
 }
