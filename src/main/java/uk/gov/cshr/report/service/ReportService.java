dd2d18a865d36ff0b079465349b66c7a8cf5f742
mmackiewicz1 <m.mackiewicz@kainos.com>
2020-06-04 15:51:05 +0200

    Implemented logging

--- src/main/java/uk/gov/cshr/report/service/ReportService.java
+++ src/main/java/uk/gov/cshr/report/service/ReportService.java
@@ -44,13 +44,16 @@ public class ReportService {
         Map<String, Event> eventMap = learningCatalogueService.getEventMap();
         Map<String, Identity> identitiesMap = dbRepository.getIdentitiesMap();
 
+        System.out.println("Event map keys:");
+        System.out.println(eventMap.keySet().toString());
+
         for (Booking booking : bookings) {
-            System.out.println("Learner id: " + booking.getLearner());
-            System.out.println("Contains learner record? " + civilServantMap.containsKey(booking.getLearner()));
             if (civilServantMap.containsKey(booking.getLearner())) {
                 String eventUid = Paths.get(booking.getEvent()).getFileName().toString();
                 Identity identity = identitiesMap.get(booking.getLearner());
 
+                System.out.println("Event uid: " + eventUid);
+                System.out.println("eventMap contains eventUid? " + eventMap.containsKey(eventUid));
                 if (eventMap.containsKey(eventUid)) {
                     Optional<CivilServant> civilServant = Optional.ofNullable(civilServantMap.get(booking.getLearner()));
                     Optional<Event> event = Optional.ofNullable(eventMap.get(eventUid));
