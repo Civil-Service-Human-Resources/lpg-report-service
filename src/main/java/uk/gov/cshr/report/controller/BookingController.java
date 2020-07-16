fe98d1c5e931aaa931228d93cbf29fc02a09738b
mmackiewicz1 <m.mackiewicz@kainos.com>
2020-07-16 12:57:17 +0200

    Implemented logging

--- src/main/java/uk/gov/cshr/report/controller/BookingController.java
+++ src/main/java/uk/gov/cshr/report/controller/BookingController.java
@@ -1,5 +1,14 @@
 package uk.gov.cshr.report.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import uk.gov.cshr.report.reports.BookingReportRow;
+import uk.gov.cshr.report.service.ReportService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -8,16 +17,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import uk.gov.cshr.report.reports.BookingReportRow;
-import uk.gov.cshr.report.service.ReportService;
-
-import java.time.LocalDate;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 @Controller
 @RequestMapping("/bookings")
 public class BookingController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BookingController.class);
+
     private final ReportService reportService;
 
     public BookingController(ReportService reportService) {
@@ -29,6 +34,7 @@ public class BookingController {
             @RequestParam("from") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
             @RequestParam("to") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to, Authentication authentication
     ) throws ExecutionException, InterruptedException {
+        LOGGER.info("Generating booking report by report by user with ID \"{}\", from \"{}\" to \"{}\"", authentication.getPrincipal().toString(), from.toString(), to.toString());
         boolean isProfessionReporter = authentication.getAuthorities().contains(new SimpleGrantedAuthority("PROFESSION_REPORTER"));
         List<BookingReportRow> bookingReport = reportService.buildBookingReport(from, to, isProfessionReporter);
         return ResponseEntity.ok(bookingReport);
--- src/main/java/uk/gov/cshr/report/controller/ModuleController.java
+++ src/main/java/uk/gov/cshr/report/controller/ModuleController.java
@@ -1,5 +1,14 @@
 package uk.gov.cshr.report.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import uk.gov.cshr.report.reports.ModuleReportRow;
+import uk.gov.cshr.report.service.ReportService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -8,16 +17,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import uk.gov.cshr.report.reports.ModuleReportRow;
-import uk.gov.cshr.report.service.ReportService;
-
-import java.time.LocalDate;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 @RestController
 @RequestMapping("/modules")
 public class ModuleController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ModuleController.class);
 
     private final ReportService reportService;
 
@@ -31,6 +35,7 @@ public class ModuleController {
             @RequestParam("to") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to,
             Authentication authentication
     ) throws ExecutionException, InterruptedException {
+        LOGGER.info("Generating learner record report by user with ID \"{}\", from \"{}\" to \"{}\"", authentication.getPrincipal().toString(), from.toString(), to.toString());
         boolean isProfessionReporter = authentication.getAuthorities().contains(new SimpleGrantedAuthority("PROFESSION_REPORTER"));
         List<ModuleReportRow> report = reportService.buildModuleReport(from, to, isProfessionReporter);
 
