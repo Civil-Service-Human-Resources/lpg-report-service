743e478f046df2e5b08c62615edc8b35514ea269
Marcin Mackiewicz <m.mackiewicz@kainos.com>
2020-07-16 13:33:16 +0200

    Implemented logging (#43)
    Add user and report type logging on report generation

--- src/main/java/uk/gov/cshr/report/controller/BookingController.java
+++ src/main/java/uk/gov/cshr/report/controller/BookingController.java
@@ -1,5 +1,13 @@
 package uk.gov.cshr.report.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import lombok.extern.slf4j.Slf4j;
+import uk.gov.cshr.report.reports.BookingReportRow;
+import uk.gov.cshr.report.service.ReportService;
+
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -8,13 +16,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import uk.gov.cshr.report.reports.BookingReportRow;
-import uk.gov.cshr.report.service.ReportService;
 
-import java.time.LocalDate;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-
+@Slf4j
 @Controller
 @RequestMapping("/bookings")
 public class BookingController {
@@ -29,6 +32,7 @@ public class BookingController {
             @RequestParam("from") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
             @RequestParam("to") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to, Authentication authentication
     ) throws ExecutionException, InterruptedException {
+        log.info("Generating booking report by user with ID \"{}\", from \"{}\" to \"{}\"", authentication.getPrincipal(), from, to);
         boolean isProfessionReporter = authentication.getAuthorities().contains(new SimpleGrantedAuthority("PROFESSION_REPORTER"));
         List<BookingReportRow> bookingReport = reportService.buildBookingReport(from, to, isProfessionReporter);
         return ResponseEntity.ok(bookingReport);
--- src/main/java/uk/gov/cshr/report/controller/ModuleController.java
+++ src/main/java/uk/gov/cshr/report/controller/ModuleController.java
@@ -1,5 +1,13 @@
 package uk.gov.cshr.report.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import lombok.extern.slf4j.Slf4j;
+import uk.gov.cshr.report.reports.ModuleReportRow;
+import uk.gov.cshr.report.service.ReportService;
+
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -8,17 +16,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import uk.gov.cshr.report.reports.ModuleReportRow;
-import uk.gov.cshr.report.service.ReportService;
 
-import java.time.LocalDate;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-
+@Slf4j
 @RestController
 @RequestMapping("/modules")
 public class ModuleController {
-
     private final ReportService reportService;
 
     public ModuleController(ReportService reportService) {
@@ -31,6 +33,7 @@ public class ModuleController {
             @RequestParam("to") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to,
             Authentication authentication
     ) throws ExecutionException, InterruptedException {
+        log.info("Generating learner record report by user with ID \"{}\", from \"{}\" to \"{}\"", authentication.getPrincipal(), from, to);
         boolean isProfessionReporter = authentication.getAuthorities().contains(new SimpleGrantedAuthority("PROFESSION_REPORTER"));
         List<ModuleReportRow> report = reportService.buildModuleReport(from, to, isProfessionReporter);
 
