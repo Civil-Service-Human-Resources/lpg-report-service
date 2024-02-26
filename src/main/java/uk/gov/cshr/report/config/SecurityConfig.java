72447ac05b32e8969a7ea96d31532f54d1a35654
SavvasCO <savvas.stephanides@cabinetoffice.gov.uk>
2024-02-26 16:56:02 +0000

    Update

--- src/main/java/uk/gov/cshr/report/config/SecurityConfig.java
+++ src/main/java/uk/gov/cshr/report/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package uk.gov.cshr.report.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
@@ -18,16 +20,34 @@ public class SecurityConfig {
     @Value("${oauth.jwtKey}")
     private String jwtKey;
 
+    private final CustomBasicAuthenticationProvider basicAuthenticationProvider;
+
+    public SecurityConfig(CustomBasicAuthenticationProvider basicAuthenticationProvider) {
+        this.basicAuthenticationProvider = basicAuthenticationProvider;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-                .oauth2ResourceServer().jwt(jwtSpec -> jwtSpec.decoder(jwtDecoder()))
+        return httpSecurity.cors().and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().oauth2ResourceServer().jwt(jwtSpec -> jwtSpec.decoder(jwtDecoder()))
+                .and().authorizeHttpRequests().anyRequest().permitAll()
+                .and().httpBasic()
+                .and().oauth2ResourceServer().jwt(jwtSpec -> jwtSpec.decoder(jwtDecoder()))
                 .and().build();
     }
 
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.authenticationProvider(basicAuthenticationProvider);
+        return authenticationManagerBuilder.build();
     }
 
     @Bean
