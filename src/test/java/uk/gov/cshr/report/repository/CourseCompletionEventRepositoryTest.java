f99180af21697c3f17908d52a1172b08af71d41a
Jamie <jamie.lovell@cabinetoffice.gov.uk>
2024-04-04 14:01:45 +0100

    course completions in code

--- src/test/java/uk/gov/cshr/report/repository/CourseCompletionEventRepositoryTest.java
+++ src/test/java/uk/gov/cshr/report/repository/CourseCompletionEventRepositoryTest.java
@@ -24,6 +24,7 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,6 +39,7 @@ public class CourseCompletionEventRepositoryTest extends IntegrationTestBase {
 
     @Autowired
     private ObjectMapper objectMapper;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     private void assertAggregation(CourseCompletionAggregation aggregation, Instant expTimestamp,
                                    String expCourseId, Integer expCount) {
@@ -46,11 +48,21 @@ public class CourseCompletionEventRepositoryTest extends IntegrationTestBase {
         assertEquals(expCount, aggregation.getTotal());
     }
 
+    private LocalDateTime parseTime(String dateTimeString) {
+        return LocalDateTime.parse(dateTimeString, formatter);
+    }
+
     @Test
-    public void testGetCourseAggregations() throws IOException {
-        File testFile = ResourceUtils.getFile(getClass().getResource("/data/sample-completions.json"));
-        List<CourseCompletionEvent> completionEvents = objectMapper.readValue(testFile, new TypeReference<>() {});
-        courseCompletionEventRepository.saveAll(completionEvents);
+    public void testGetCourseAggregations(){
+        List<CourseCompletionEvent> cces = List.of(
+                new CourseCompletionEvent("MESSAGEID0", "user1", "user1@cabinetoffice.gov.uk", "course1", "Health and Safety", parseTime("2024-01-01 09:20:00"), 1, 2, 3),
+                new CourseCompletionEvent("MESSAGEID1", "user1", "user1@cabinetoffice.gov.uk", "course2", "Responsible for information", parseTime("2024-01-01 10:40:00"), 1, 2, 3),
+                new CourseCompletionEvent("MESSAGEID2", "user2", "user2@cabinetoffice.gov.uk", "course1", "Health and Safety", parseTime("2024-01-01 15:15:00"), 1, 2, 3),
+                new CourseCompletionEvent("MESSAGEID3", "user2", "user2@cabinetoffice.gov.uk", "course2", "Responsible for information", parseTime("2024-01-01 18:09:00"), 1, 2, 3),
+                new CourseCompletionEvent("MESSAGEID4", "user3", "user3@cabinetoffice.gov.uk", "course1", "Health and Safety", parseTime("2024-01-01 09:30:00"), 1, 2, 3),
+                new CourseCompletionEvent("MESSAGEID5", "user4", "user4@cabinetoffice.gov.uk", "course1", "Health and Safety", parseTime("2024-01-01 19:30:00"), 1, 2, 3)
+        );
+        courseCompletionEventRepository.saveAll(cces);
 
         CourseCompletionAggregation[] completionsAggregationByCourse = courseCompletionEventRepository.getCompletionsAggregationByCourse("hour",
                 LocalDateTime.of(2024, 1, 1, 0, 0),
