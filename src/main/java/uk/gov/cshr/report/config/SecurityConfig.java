7288b554c65ac333bb810feb379636d3c69bcc39
Jamie <jamie.lovell@cabinetoffice.gov.uk>
2024-04-08 13:40:49 +0100

    Swagger and security chain refactoring

--- src/main/java/uk/gov/cshr/report/config/SecurityConfig.java
+++ src/main/java/uk/gov/cshr/report/config/SecurityConfig.java
@@ -1,9 +1,13 @@
 package uk.gov.cshr.report.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
@@ -14,18 +18,34 @@ import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
+@Slf4j
 public class SecurityConfig {
     @Value("${oauth.jwtKey}")
     private String jwtKey;
 
     @Bean
+    @Order(1)
+    public SecurityFilterChain basicAuthChain(HttpSecurity httpSecurity) throws Exception {
+        log.info("Building basic auth filter chain");
+        return httpSecurity.securityMatcher("/swagger-ui/**", "/v3/api-docs/**")
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    @Order(2)
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.cors().and().csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().oauth2ResourceServer().jwt(jwtSpec -> jwtSpec.decoder(jwtDecoder()))
-                .and().authorizeHttpRequests().anyRequest().permitAll()
-                .and().httpBasic()
-                .and().build();
+        return httpSecurity.cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(httpSecurityOAuth2ResourceServerConfigurer -> httpSecurityOAuth2ResourceServerConfigurer.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder())))
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry.anyRequest().permitAll())
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .build();
     }
 
     @Bean
