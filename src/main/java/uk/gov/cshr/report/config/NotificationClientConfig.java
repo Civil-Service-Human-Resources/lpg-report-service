1d040b339b888ee5e021ff58969312b9daf2fd29
Jamie <jamie.lovell@cabinetoffice.gov.uk>
2024-08-23 11:47:56 +0100

    OAuth interceptor on notification service client

--- src/main/java/uk/gov/cshr/report/config/NotificationClientConfig.java
+++ src/main/java/uk/gov/cshr/report/config/NotificationClientConfig.java
@@ -7,16 +7,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import uk.gov.cshr.report.client.HttpClient;
 import uk.gov.cshr.report.client.IHttpClient;
+import uk.gov.cshr.report.service.auth.RestTemplateOAuthInterceptor;
 
 @Configuration
 public class NotificationClientConfig {
     @Value("${notification.serviceUrl}")
     private String notificationServiceUrl;
 
+    private final RestTemplateOAuthInterceptor restTemplateOAuthInterceptor;
+
+    public NotificationClientConfig(RestTemplateOAuthInterceptor restTemplateOAuthInterceptor) {
+        this.restTemplateOAuthInterceptor = restTemplateOAuthInterceptor;
+    }
+
     @Bean(name = "notificationHttpClient")
     IHttpClient notificationClient(RestTemplateBuilder restTemplateBuilder){
         RestTemplate restTemplate = restTemplateBuilder
                 .rootUri(notificationServiceUrl)
+                .additionalInterceptors(restTemplateOAuthInterceptor)
                 .build();
 
         return new HttpClient(restTemplate);
