fa23d0001280eb301d910fc0875b60646296d33e
Jamie <jamie.lovell@cabinetoffice.gov.uk>
2025-01-23 08:49:21 +0000

    Course IDs null, fix querying with null course IDs

--- src/main/java/uk/gov/cshr/report/service/CourseCompletionsParamsFactory.java
+++ src/main/java/uk/gov/cshr/report/service/CourseCompletionsParamsFactory.java
@@ -4,12 +4,17 @@ import org.springframework.stereotype.Service;
 import uk.gov.cshr.report.controller.model.GetCourseCompletionsByCourseParams;
 import uk.gov.cshr.report.domain.report.CourseCompletionReportRequest;
 
+import java.util.List;
+
+import static org.springframework.util.CollectionUtils.isEmpty;
+
 @Service
 public class CourseCompletionsParamsFactory {
 
     public GetCourseCompletionsByCourseParams fromReportRequest(CourseCompletionReportRequest request){
         GetCourseCompletionsByCourseParams params = new GetCourseCompletionsByCourseParams();
-        params.setCourseIds(request.getCourseIds());
+        List<String> courseIds = request.getCourseIds();
+        params.setCourseIds(isEmpty(courseIds) ? null : courseIds);
         params.setOrganisationIds(request.getOrganisationIds());
         params.setStartDate(request.getFromDate().toLocalDateTime());
         params.setEndDate(request.getToDate().toLocalDateTime());
