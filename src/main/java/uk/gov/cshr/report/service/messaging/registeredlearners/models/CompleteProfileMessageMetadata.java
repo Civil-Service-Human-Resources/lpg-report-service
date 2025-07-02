42ef4522f75fe96a72d06bcd500e7e108c3110a0
Jamie <jamie.lovell@cabinetofice.gov.uk>
2025-07-02 12:27:53 +0100

    AllArgsConstructor

--- src/main/java/uk/gov/cshr/report/service/messaging/registeredlearners/models/CompleteProfileMessageMetadata.java
+++ src/main/java/uk/gov/cshr/report/service/messaging/registeredlearners/models/CompleteProfileMessageMetadata.java
@@ -1,12 +1,16 @@
 package uk.gov.cshr.report.service.messaging.registeredlearners.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serial;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public final class CompleteProfileMessageMetadata extends RegisteredLearnerMetadata<RegisteredLearnerProfile> {
     @Serial
     private static final long serialVersionUID = 0L;
