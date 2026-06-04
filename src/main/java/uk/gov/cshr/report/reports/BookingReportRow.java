71d35c0c5837bc7da87f55d4252feb92aa33846f
jamie <jamie.lovell@cabinetoffice.gov.uk>
2026-06-04 16:17:59 +0100

    CSV ordering

--- src/main/java/uk/gov/cshr/report/reports/BookingReportRow.java
+++ src/main/java/uk/gov/cshr/report/reports/BookingReportRow.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
 
-@JsonPropertyOrder({"learnerId", "name", "email", "departmentCode", "department", "profession", "otherAreasOfWork", "grade", "courseId",
+@JsonPropertyOrder({"learnerId", "name", "email", "department", "departmentCode", "profession", "otherAreasOfWork", "grade", "courseId",
         "courseTitle", "moduleId", "moduleTitle", "learningProvider", "required", "associatedLearning", "status", "bookingTime", "confirmationTime",
         "cancellationTime", "accessibilityOptions", "bookingCancellationReason", "topicId", "poNumber", "eventId", "bookingReference", "location", "eventDate"})
 @JsonInclude(JsonInclude.Include.NON_NULL)
