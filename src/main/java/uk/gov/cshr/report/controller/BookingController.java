e92d34a268187078bd1ae9669ee65971e07142f9
jamie <jamie.lovell@cabinetoffice.gov.uk>
2026-06-08 09:02:20 +0100

    Switch to IUserAuthService for auth details

--- src/main/java/uk/gov/cshr/report/controller/BookingController.java
+++ src/main/java/uk/gov/cshr/report/controller/BookingController.java
@@ -3,11 +3,10 @@ package uk.gov.cshr.report.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.web.bind.annotation.*;
 import uk.gov.cshr.report.reports.BookingReportRow;
 import uk.gov.cshr.report.service.ReportService;
+import uk.gov.cshr.report.service.auth.IUserAuthService;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -17,23 +16,24 @@ import java.util.List;
 @RequestMapping("/bookings")
 public class BookingController {
     private final ReportService reportService;
+    private final IUserAuthService userAuthService;
 
-    public BookingController(ReportService reportService) {
+    public BookingController(ReportService reportService, IUserAuthService userAuthService) {
         this.reportService = reportService;
+        this.userAuthService = userAuthService;
     }
 
     @GetMapping(produces = "application/csv; charset=utf-8", params = {"from", "to"})
     public ResponseEntity<List<BookingReportRow>> generateBookingReport(
             @RequestParam("from") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
             @RequestParam("to") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to,
-            @RequestHeader(required = false) Integer organisationId,
-            Authentication authentication
+            @RequestHeader(required = false) Integer organisationId
     ) {
-        log.info("Generating booking report for user ID \"{}\", from \"{}\" to \"{}\"", authentication.getPrincipal(), from, to);
-        boolean isCshrReporter = authentication.getAuthorities().contains(new SimpleGrantedAuthority("CSHR_REPORTER"));
+        log.info("Generating booking report for user ID \"{}\", from \"{}\" to \"{}\"", userAuthService.getUsername(), from, to);
+        boolean isCshrReporter = userAuthService.userHasRole("CSHR_REPORTER");
         organisationId = isCshrReporter ? null : organisationId;
         List<BookingReportRow> bookingReport = reportService.buildBookingReport(from, to, organisationId);
-        log.info("Booking report generated for user ID \"{}\", from \"{}\" to \"{}\"", authentication.getPrincipal(), from, to);
+        log.info("Booking report generated for user ID \"{}\", from \"{}\" to \"{}\"", userAuthService.getUsername(), from, to);
         return ResponseEntity.ok(bookingReport);
     }
 }
